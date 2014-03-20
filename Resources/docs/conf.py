17e84309ce10da855dd061f742c5e277ec017162
Marc Morera <yuhu@mmoreram.com>
2014-03-21 00:43:04 +0100

    Created conf.py for readthedocs

---
+++ Resources/docs/conf.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+source_suffix = '.rst'
+master_doc = 'index'
+project = 'Payment Suite'
+copyright = u'2013-2014, Marc Morera'
+version = ''
+release = ''
+html_theme = 'default'
