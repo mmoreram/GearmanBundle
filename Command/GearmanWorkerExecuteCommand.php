919beb90eb01843d38739bbd413cc225656941b0
DZunke <denis.zunke@googlemail.com>
2014-05-19 14:34:56 +0200

    --quiet should also work like individual --no-description

--- Command/GearmanWorkerExecuteCommand.php
+++ Command/GearmanWorkerExecuteCommand.php
@@ -62,8 +62,7 @@ class GearmanWorkerExecuteCommand extends ContainerAwareCommand
         $worker = $input->getArgument('worker');
         $workerStruct = $this->getContainer()->get('gearman')->getWorker($worker);
 
-        if (!$input->getOption('no-description')) {
-
+        if (!$input->getOption('no-description') || !$input->getOption('quiet')) {
             $this->getContainer()->get('gearman.describer')->describeWorker($output, $workerStruct, true);
         }
 
