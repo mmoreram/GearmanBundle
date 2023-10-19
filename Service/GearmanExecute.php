1891fc877e8c348fec4a54b0506e024c3e29724a
BlueTM <662454+BlueTM@users.noreply.github.com>
2023-10-19 13:43:31 +0300

    Make it compatible with 7.4

--- Service/GearmanExecute.php
+++ Service/GearmanExecute.php
@@ -95,7 +95,7 @@ class GearmanExecute extends AbstractGearmanService
     /**
      * Toggles that work should be stopped, we only subscribe to SIGTERM,SIGINT and SIGHUP
      */
-    public function handleSystemSignal(int $signo, mixed $siginfo): void
+    public function handleSystemSignal(int $signo): void
     {
         $this->stopWorkSignalReceived = true;
     }
